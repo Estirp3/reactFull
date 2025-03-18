016fdbedc741cc94236bc93b67f465415eda2fb5
patricio.calderon <patricio.calderon@cencosudscotiabank.cl>
2025-03-18 16:47:21 -0300

    Se sube y camvia version

--- 03-counter-app-vit/src/main.jsx
+++ 03-counter-app-vit/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDom from 'react-dom/client';
-
-import { App } from "./App"
+// import { FirstApp } from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 import './style.css'
 
 
 
 ReactDom.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-       <App title={123} /> {/* Esto debería generar una advertencia */}
+        <CounterApp value={ 20 }  />
     </React.StrictMode>
 )
 
