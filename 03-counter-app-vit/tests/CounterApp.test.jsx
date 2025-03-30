2c552215b5732c4a1c6e72fd67368a621f7e2950
Patricio <patricio.calderonig@gmail.com>
2025-03-30 04:40:36 -0300

    Nueva seccion

--- 03-counter-app-vit/tests/CounterApp.test.jsx
+++ 03-counter-app-vit/tests/CounterApp.test.jsx
@@ -1,7 +1,7 @@
 //Tarea
 // Pruebas en el CounterApp
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 //**
@@ -19,6 +19,29 @@ describe("Pruebas en el CounterApp", () => {
         expect( screen.getByText( value )).toBeTruthy();//Existe      
         expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain(value.toString()); // ocupamos el toString() para que se vea que es un numero
     })
-    
-    
-});
+    test('Debe mostrar el valor 100', () => {
+        render( <CounterApp value={100} />)
+        expect( screen.getByText(value)).toBeTruthy();
+        expect( screen.getByRole('heading', {level:2}).innerHTML).toContain('100')
+    })
+    test('debe de incrementar con el btn + 1', () => {
+    render( <CounterApp value={10}/>)
+    fireEvent.click( screen.getByText('+1') )
+    expect(screen.getByText('11') ).toBeTruthy();
+    });
+    test('debe de decrementar con el btn - 1', () => {
+        render( <CounterApp value={10}/>)
+        fireEvent.click( screen.getByText('-1') )
+        expect(screen.getByText('9') ).toBeTruthy();
+    });
+    test('debe de resetear el valor al presionar el btn reset', () => {
+        render( <CounterApp value={10}/>)
+        fireEvent.click( screen.getByText('+1') )
+        fireEvent.click( screen.getByText('+1') )
+        fireEvent.click( screen.getByText('+1') )
+        // fireEvent.click( screen.getByText('Reset') )
+        // expect(screen.getByText('10') ).toBeTruthy();
+        fireEvent.click(screen.getByRole('button',{name:'btn-reset'}))
+        expect(screen.getByText('10') ).toBeTruthy();
+    });
+})
